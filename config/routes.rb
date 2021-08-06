cad37b4703e9a7161f94b85a40be0cd104b0938c
Miranda Wilson <miranda@snowplowanalytics.com>
2021-08-06 10:56:52 +0100

    Create ShopController and individual item pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,20 @@ Rails.application.routes.draw do
 
   get "home/index"
   get "home/about"
-  get "home/shop"
 
-  post "track_page_view", to: "home#track_page_view"
+  get 'shop/all_products'
+  get 'shop/blue_skis'
+  get 'shop/green_skis'
+  get 'shop/brown_skis'
+  get 'shop/white_poles'
+  get 'shop/confirmation'
+
+  post "track_page_view", to: "application#track_page_view"
+
   post "track_screen_view", to: "home#track_screen_view"
   post "track_ecommerce", to: "home#track_ecommerce"
   post "track_struct", to: "home#track_struct"
   post "track_self_describing", to: "home#track_self_describing"
+
+  post "add_to_basket", to: "shop#add_to_basket"
 end
