0d650bb7fceb0192004e9bc71549b7aeb89489f8
Miranda Wilson <miranda@snowplowanalytics.com>
2021-08-10 16:24:26 +0100

    Display item names in basket

--- app/javascript/shop.js
+++ app/javascript/shop.js
@@ -1,7 +1,7 @@
 console.log("in Shop.js");
 
 document.addEventListener("turbolinks:load", function () {
-  updateTotal();
+  updateTotal(0);
   updateCount();
 
   const addToBasketButtons = document.querySelectorAll(".basket-add");
@@ -9,11 +9,21 @@ document.addEventListener("turbolinks:load", function () {
   addToBasketButtons.forEach((element) => {
     element.addEventListener("click", () => {
       console.log("click!");
-      let itemPrice = parseFloat(element.attributes[1].nodeValue);
-      basketTotal += itemPrice;
-      basketCount += 1;
-      updateTotal();
+
+      const price = parseFloat(
+        element.attributes.getNamedItem("data-price").nodeValue
+      );
+      const name = element.form.elements.name.value;
+      const sku = element.form.elements.sku.value;
+      const isOnSale = element.form.elements.on_sale.value;
+      const originalPrice = element.form.elements.original_price.value;
+
+      // console.log(name);
+      // console.log(element.form.elements);
+
+      updateTotal(price);
       updateCount();
+      addItemToBasket(name);
     });
   });
 });
@@ -21,16 +31,19 @@ document.addEventListener("turbolinks:load", function () {
 let basketTotal = 0;
 let basketCount = 0;
 
-function updateTotal() {
-  let total = document.getElementById("order-total");
+function updateTotal(price) {
+  basketTotal += price;
+  const total = document.getElementById("order-total");
   total.innerHTML = basketTotal;
 }
 
 function updateCount() {
-  let amount = document.getElementById("order-count");
+  basketCount += 1;
+  const amount = document.getElementById("order-count");
   amount.innerHTML = basketCount;
 }
 
-function updateItems() {}
-
-function addItemToBasket(item) {}
+function addItemToBasket(name) {
+  const basketContents = document.getElementById("basket-items");
+  basketContents.insertAdjacentHTML("afterbegin", `<li>${name}</li>`);
+}
